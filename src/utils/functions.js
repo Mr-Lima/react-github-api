96b676c9d206a7cffd1fdd1a8e411155e583dc4d
Andre Lima <dev.andrelima@gmail.com>
2021-03-08 01:03:07 -0300

    note

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,5 +1,7 @@
 export function requestError(err) {
   console.log(err);
-  alert('erro requisicao');
+  alert(
+    'erro requisicao, lembre-se que o github restringe o numero de chamadas caso voce nao esteja logado'
+  );
   window.location.href = '/';
 }
